f513b409d90b6907ac7d9c0c1586d73c0c776073
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-10-29 21:22:55 +0300

    Update task6.cs

--- practice4/task6.cs
+++ practice4/task6.cs
@@ -1,1 +1,36 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+      int D = 5; // для примера
+      int K = 3; // для примера 
+      int[] A = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+      
+      List<int> union = new List<int>();
+      HashSet<int> uniqueValues = new HashSet<int>();
+      
+      for (int i = 0; i < A.Length; i++)
+      {
+          if (A[i] >= D)
+          {
+              break;
+          }
+          uniqueValues.Add(A[i]);
+      }
+      
+      for (int i = K - 1; i < A.Length; i++)
+      {
+          uniqueValues.Add(A[i]);
+      }
+      
+      union.AddRange(uniqueValues);
+      union.Sort((a, b) => b.CompareTo(a));
+      
+      foreach (int num in union)
+      {
+          Console.WriteLine(num);
+      }
+    }
+}
