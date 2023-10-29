383fd01b616d1de65edf1ec0eabe8288c2ab9003
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-10-29 21:01:18 +0300

    Update 2task.cs

--- 2task.cs
+++ 2task.cs
@@ -1,15 +1,24 @@
-int D = 5; // Замените D на нужное однозначное целое число
-int[] A = { 12, 35, 45, 18, 27, 0 };
+using System;
+using System.Linq;
 
-int result = 0;
-
-foreach (int num in A)
+class Program
 {
-    if (num > 0 && num % 10 == D)
+    static void Main()
     {
-        result = num;
-        break;
+    int D = 5; // Замените D на нужное однозначное целое число
+    int[] A = { 12, 35, 45, 18, 27, 0 };
+
+    int result = 0;
+
+    foreach (int num in A)
+    {
+        if (num > 0 && num % 10 == D)
+        {
+            result = num;
+            break;
+        }
+    }
+
+    Console.WriteLine(result != 0 ? result.ToString() : "0");
     }
 }
-
-Console.WriteLine(result != 0 ? result.ToString() : "0");
