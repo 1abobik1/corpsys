e705962454a54394d5d05800226b35fbc8431d4c
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-10-29 21:00:40 +0300

    Create 2task.cs

---
+++ 2task.cs
@@ -0,0 +1,15 @@
+int D = 5; // Замените D на нужное однозначное целое число
+int[] A = { 12, 35, 45, 18, 27, 0 };
+
+int result = 0;
+
+foreach (int num in A)
+{
+    if (num > 0 && num % 10 == D)
+    {
+        result = num;
+        break;
+    }
+}
+
+Console.WriteLine(result != 0 ? result.ToString() : "0");
