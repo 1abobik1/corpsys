51207b66526a2acc28d835d2287394ffaf03778f
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-10-29 21:22:23 +0300

    Update task3.cs

--- practice4/task3.cs
+++ practice4/task3.cs
@@ -1,1 +1,24 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+      int L = 3; // можно любое другое число..
+      string[] A = { "Apple", "123abc", "45defg", "Xyz987" };
+    
+      string result = "Not found";
+      
+      for (int i = A.Length - 1; i >= 0; i--)
+      {
+          if (A[i].Length == L && Char.IsDigit(A[i][0]))
+          {
+              result = A[i];
+              break;
+          }
+      }
+      
+      Console.WriteLine(result);
+
+    }
+}
