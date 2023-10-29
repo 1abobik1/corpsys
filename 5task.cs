7d121e5bb817b8c822ee44e9bf083389d3b772e5
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-10-29 21:07:44 +0300

    Create 5task.cs

---
+++ 5task.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+      int D = 5; // Замените D на нужное целое число
+      int[] A = { 2, 7, 4, 9, 12, 6, 15, 8 };
+      
+      List<int> result = new List<int>();
+      
+      bool foundGreaterD = false;
+      
+      for (int i = 0; i < A.Length; i++)
+      {
+          if (A[i] > D)
+          {
+              foundGreaterD = true;
+          }
+      
+          if (foundGreaterD && A[i] % 2 != 0 && A[i] > 0)
+          {
+              result.Add(A[i]);
+          }
+      }
+      
+      result.Reverse();
+      
+      foreach (int num in result)
+      {
+          Console.WriteLine(num);
+      }
+
+    }
+}
