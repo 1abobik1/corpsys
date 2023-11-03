5383c55988203eac104747ba19c2caa4e82b2b52
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-11-03 09:32:29 +0300

    Update task2.cs

--- practice4/task2.cs
+++ practice4/task2.cs
@@ -1,22 +1,21 @@
 using System;
+using System.Linq;
+
+
 class Program
 {
     static void Main()
     {
-    int D = 5; // можно любое другое число..
-    int[] A = { 12, 35, 45, 18, 27, 0 };
+        int[] sequence = { 15, 24, 37, 42, 53, 68, 79, 100 };
+        int digitD = 7; // Заданная цифра D
 
-    int result = 0;
+        int result = sequence.FirstOrDefault(x => x > 0 && x % 10 == digitD);
 
-    foreach (int num in A)
-    {
-        if (num > 0 && num % 10 == D)
-        {
-            result = num;
-            break;
-        }
-    }
-
-    Console.WriteLine(result != 0 ? result.ToString() : "0");
+        if (result != 0)
+            Console.WriteLine("First positive number ending with " + digitD + ": " + result);
+        else
+            Console.WriteLine("No matching element found in the sequence.");
     }
 }
+
+
