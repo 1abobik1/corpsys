c653ff9fad543ac244434d5800e8fd26ef606c74
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-11-03 09:39:52 +0300

    Update task7.cs

--- practice4/task7.cs
+++ practice4/task7.cs
@@ -1,41 +1,25 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-      int K = 3; // для примера
-      int[] A = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-      
-      List<int> difference = new List<int>();
-      HashSet<int> uniqueValues = new HashSet<int>();
-      bool foundK = false;
-      
-      for (int i = 0; i < A.Length; i++)
-      {
-          if (A[i] % 2 == 0)
-          {
-              uniqueValues.Add(A[i]);
-          }
-      
-          if (i + 1 > K)
-          {
-              foundK = true;
-          }
-      
-          if (foundK)
-          {
-              uniqueValues.Remove(A[i]);
-          }
-      }
-      
-      difference.AddRange(uniqueValues);
-      difference.Reverse();
-      
-      foreach (int num in difference)
-      {
-          Console.WriteLine(num);
-      }
+        int[] sequence = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int K = 5; // Заданное число K
 
+        var firstFragment = sequence.Where(x => x % 2 == 0); // Выбираем все четные числа
+        var secondFragment = sequence.Skip(K); // Пропускаем первые K элементов
+
+        var difference = firstFragment.Except(secondFragment).Reverse();
+
+        if (difference.Any())
+        {
+            Console.WriteLine("Difference of two fragments: " + string.Join(", ", difference));
+        }
+        else
+        {
+            Console.WriteLine("No matching elements found.");
+        }
     }
 }
