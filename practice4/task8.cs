58eceff49a2f420db26aba109c3e8eeb9d846c41
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2023-11-03 09:49:13 +0300

    Update task8.cs

--- practice4/task8.cs
+++ practice4/task8.cs
@@ -1,42 +1,31 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-      int K = 2; // можно любое другое число..
-      string[] A = { "ABC123", "DEF456", "GHI789", "JKL", "MNOXYZ", "PQR123", "STU" };
-      
-      List<string> intersection = new List<string>();
-      HashSet<string> uniqueValues = new HashSet<string>();
-      bool foundLastDigit = false;
-      
-      for (int i = 0; i < A.Length; i++)
-      {
-          if (i < 3 * K)
-          {
-              uniqueValues.Add(A[i]);
-          }
-      
-          if (!foundLastDigit && A[i].Any(char.IsDigit) && char.IsDigit(A[i].Last()))
-          {
-              foundLastDigit = true;
-          }
-      }
-      
-      for (int i = A.Length - 1; i >= 0; i--)
-      {
-          if (foundLastDigit && i >= 3 * K)
-          {
-              uniqueValues.Add(A[i]);
-          }
-      }
-      
-      intersection.AddRange(uniqueValues.OrderBy(s => s.Length).ThenBy(s => s));
-      
-      foreach (string str in intersection)
-      {
-          Console.WriteLine(str);
-      }
+        string[] sequence = { "Apple123", "Banana456", "Carrot789", "Dog123", "Elephant456", "Fish789", "Giraffe123" };
+        int K = 2; // Заданное число K
+
+        var firstFragment = sequence.Take(3 * K); // Выбираем первые 3K элементов
+        var lastDigit = sequence.Last(str => char.IsDigit(str.Last())); // Находим последний элемент, оканчивающийся цифрой
+        var secondFragment = sequence.SkipWhile(str => str != lastDigit).Skip(1); // Пропускаем элементы до последнего элемента и сам последний элемент
+
+        var intersection = firstFragment.Intersect(secondFragment).Distinct(); // Выполняем теоретико-множественное пересечение и удаляем дубликаты
+
+        var sortedIntersection = intersection
+            .OrderBy(str => str.Length) // Сортируем по возрастанию длин строк
+            .ThenBy(str => str); // Затем сортируем в лексикографическом порядке по возрастанию
+
+        if (sortedIntersection.Any())
+        {
+            Console.WriteLine("Intersection of two fragments: " + string.Join(", ", sortedIntersection));
+        }
+        else
+        {
+            Console.WriteLine("No matching elements found.");
+        }
     }
 }
